99dc3c4fac6335d0641591b25d9e4d512fd46bf7
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2019-12-06 11:03:37 +0300

    [#5] Disable strict dependency from odahu SDK to soft dependency (>=)

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         ]
     },
     install_requires=[
-        'odahu-flow-sdk==1.0.0rc29',
+        'odahu-flow-sdk>=1.0.0rc29',
         'requests>=2.22.0'
     ],
     extras_require={
