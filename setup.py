35421e488ec29d2fcf15040c1f02e12a3b06b92f
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2020-12-25 13:17:41 +0300

    remove workaround because of new resolver

--- setup.py
+++ setup.py
@@ -60,10 +60,7 @@ setup(
     },
     install_requires=[
         'odahu-flow-sdk==1.4.0rc1',
-        'requests>=2.22.0',
-        # because this package is in conflict of odahu-flow-sdk and requests but pip doesn't have true dependency
-        # resolver (https://github.com/pypa/pip/issues/988)
-        'urllib3>=1.24.3'
+        'requests>=2.22.0'
     ],
     extras_require={
         'testing': [
