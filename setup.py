5925ab3ff3841fdd683feed62588e52c5cb77b80
oleh_mykolaishyn <owlleg6@gmail.com>
2021-12-13 11:50:53 +0200

    migration to 2.2.1 test

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
             'pytest-mock>=1.10.4',
             'pytest-cov>=2.7.1',
             'pylint>=2.3.0',
-            'apache-airflow==1.10.14',
+            'apache-airflow==2.2.1',
             'google-api-python-client'
         ]
     },
